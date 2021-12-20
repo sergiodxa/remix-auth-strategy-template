3fe123bee64eeb845c024146d896a1f0510f1bf0
Sergio Xalambrí <hello@sergiodxa.com>
2021-12-20 02:12:12 -0500

    Create template code

---
+++ test/index.test.ts
@@ -0,0 +1,23 @@
+import { createCookieSessionStorage } from "@remix-run/server-runtime";
+import { MyStrategy } from "../src";
+
+describe(MyStrategy, () => {
+  let verify = jest.fn();
+  // You will probably need a sessionStorage to test the strategy.
+  let sessionStorage = createCookieSessionStorage({
+    cookie: { secrets: ["s3cr3t"] },
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("should have the name of the strategy", () => {
+    let strategy = new MyStrategy({ something: "You may need" }, verify);
+    expect(strategy.name).toBe(
+      "Don't forget to change me in the Strategy code"
+    );
+  });
+
+  test.todo("Write more tests to check everything works as expected");
+});
