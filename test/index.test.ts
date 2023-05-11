ae0eec6eb417a35579e0a93a41435304eb1b10ca
sur.la.route <17788706+christopherpickering@users.noreply.github.com>
2023-05-10 20:04:29 -0500

    Update index.test.ts (#4)
    The import is in /node now.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { createCookieSessionStorage } from "@remix-run/server-runtime";
+import { createCookieSessionStorage } from "@remix-run/node";
 import { MyStrategy } from "../src";
 
 describe(MyStrategy, () => {
